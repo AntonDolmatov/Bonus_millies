ce9078f6f6837fd10e65b2ed956f934c2c0f7053
Anton Dolmatov <tosha.dolmatow@yandex.ru>
2023-08-13 11:26:55 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 13676; // стоимость билета
+
+        int miles = ticketPrice / 20; // вычисляем количество миль
+
+        System.out.println("Начислено " + miles + " миль");
+    }
+}
